933fa7ee35402bde78ef8c9fb44553bbc19e29e5
Dagim Sisay <Dagim>
2025-08-17 12:05:51 +0300

    Completed Fetch Data

---
+++ Fetch Data/src/tutorial.ts
@@ -0,0 +1,42 @@
+//Fetch Data
+
+import { z } from "zod";
+
+const url = "http://www-course-api.com/react-tour-project";
+
+const tourSchema = z.object({
+  id: String(),
+  name: String(),
+  info: String(),
+  image: String(),
+  price: String(),
+});
+
+type Tour = z.infer<typeof tourSchema>;
+
+async function fetchData(url: string): Promise<Tour[]> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const rawData: Tour[] = await response.json();
+    const result = tourSchema.array().safeParse(rawData);
+
+    if (!result.success) {
+      throw new Error(`Invalid data: {result.error}`);
+    }
+
+    return result.data;
+  } catch (error) {
+    const errorMsg =
+      error instanceof Error ? error.message : "there was an error...";
+    console.log(errorMsg);
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+  console.log(tour.name);
+});
